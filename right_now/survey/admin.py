c469123d0724cecf0cb08936a6a63bd3468040f2
Zhao Ming <mzhao003@e.ntu.edu.sg>
2015-04-08 12:02:03 +0800

    multiple Qtype style

--- right_now/survey/admin.py
+++ right_now/survey/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from survey.models import Survey, Module, Question
+from survey.models import Survey, Module, Question, SurveyQuestion
+from django.forms import TextInput, Textarea
+from django.db import models
 
-admin.site.register(Survey)
 admin.site.register(Module)
 admin.site.register(Question)
 
-# Register your models here.
+class SurveyQuestionInline(admin.TabularInline):
+    model = SurveyQuestion
+
+class SurveyAdmin(admin.ModelAdmin):
+    inlines = [SurveyQuestionInline]
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':6, 'cols': 80})},
+    }
+
+admin.site.register(Survey, SurveyAdmin)
